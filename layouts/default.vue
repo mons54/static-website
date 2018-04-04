31a4b583ccf2dc9fe801098437d4170097a8ab5c
Julien Simonet <j.mons54@gmail.com>
2018-04-04 05:42:23 +0200

    add nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
+    <app-nav/>
     <nuxt/>
   </div>
 </template>
 
-<script type="text/javascript">
+<script>
+import AppNav from '~/components/AppNav.vue'
+
+export default {
+  components: {
+    AppNav
+  },
+}
+if (process.browser) {
+  const stylesheet = document.createElement('link');
+  stylesheet.href = 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons';
+  stylesheet.rel  = 'stylesheet';
+  document.getElementsByTagName('head')[0].appendChild(stylesheet);
+}
 </script>
 
-<style>
+<style lang="scss">
+@import "assets/scss/material-kit.scss";
 </style>
